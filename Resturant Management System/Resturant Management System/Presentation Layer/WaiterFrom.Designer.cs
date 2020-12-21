fbdda5b62aaeb899a1e7ff6e6b9fb46dda7a4d29
HabiburRahmanAnik <habib264521@gmail.com>
2020-12-22 00:55:03 +0600

    design WaiterFrom

--- Resturant Management System/Resturant Management System/Presentation Layer/WaiterFrom.Designer.cs
+++ Resturant Management System/Resturant Management System/Presentation Layer/WaiterFrom.Designer.cs
@@ -28,20 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.myProfileButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.takeOrderButton = new System.Windows.Forms.Button();
+            this.MenuButton = new System.Windows.Forms.Button();
+            this.logOutButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.BackgroundImage = global::Resturant_Management_System.Properties.Resources.images__3_;
+            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel3.Location = new System.Drawing.Point(3, 109);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(279, 229);
+            this.panel3.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Controls.Add(this.myProfileButton);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Location = new System.Drawing.Point(2, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(302, 473);
+            this.panel1.TabIndex = 1;
+            // 
+            // myProfileButton
+            // 
+            this.myProfileButton.BackColor = System.Drawing.Color.Maroon;
+            this.myProfileButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myProfileButton.ForeColor = System.Drawing.Color.White;
+            this.myProfileButton.Location = new System.Drawing.Point(18, 375);
+            this.myProfileButton.Name = "myProfileButton";
+            this.myProfileButton.Size = new System.Drawing.Size(264, 53);
+            this.myProfileButton.TabIndex = 2;
+            this.myProfileButton.Text = "My Profile";
+            this.myProfileButton.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Chocolate;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(29, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Waiter Dashboard";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel2.Controls.Add(this.logOutButton);
+            this.panel2.Controls.Add(this.takeOrderButton);
+            this.panel2.Controls.Add(this.MenuButton);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(310, 5);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(608, 472);
+            this.panel2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(25, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 28);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Welcome";
+            // 
+            // takeOrderButton
+            // 
+            this.takeOrderButton.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.takeOrderButton.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.takeOrderButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.takeOrderButton.Location = new System.Drawing.Point(326, 189);
+            this.takeOrderButton.Name = "takeOrderButton";
+            this.takeOrderButton.Size = new System.Drawing.Size(233, 95);
+            this.takeOrderButton.TabIndex = 4;
+            this.takeOrderButton.Text = "Take Order";
+            this.takeOrderButton.UseVisualStyleBackColor = false;
+            // 
+            // MenuButton
+            // 
+            this.MenuButton.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.MenuButton.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MenuButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.MenuButton.Location = new System.Drawing.Point(53, 189);
+            this.MenuButton.Name = "MenuButton";
+            this.MenuButton.Size = new System.Drawing.Size(232, 95);
+            this.MenuButton.TabIndex = 3;
+            this.MenuButton.Text = "Menu";
+            this.MenuButton.UseVisualStyleBackColor = false;
+            // 
+            // logOutButton
+            // 
+            this.logOutButton.BackColor = System.Drawing.Color.Maroon;
+            this.logOutButton.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logOutButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.logOutButton.Location = new System.Drawing.Point(482, 7);
+            this.logOutButton.Name = "logOutButton";
+            this.logOutButton.Size = new System.Drawing.Size(123, 40);
+            this.logOutButton.TabIndex = 5;
+            this.logOutButton.Text = "Log Out";
+            this.logOutButton.UseVisualStyleBackColor = false;
             // 
             // WaiterFrom
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(942, 450);
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(930, 476);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "WaiterFrom";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "WaiterFrom";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.WaiterFrom_FormClosing);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button myProfileButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button logOutButton;
+        private System.Windows.Forms.Button takeOrderButton;
+        private System.Windows.Forms.Button MenuButton;
+        private System.Windows.Forms.Label label2;
     }
 }
