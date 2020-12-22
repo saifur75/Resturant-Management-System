7d631e5495f739df79817716fa4053450780aa98
HabiburRahmanAnik <habib264521@gmail.com>
2020-12-22 11:32:28 +0600

    implement User class

--- Resturant Management System/Resturant Management System/Entities/User.cs
+++ Resturant Management System/Resturant Management System/Entities/User.cs
@@ -8,5 +8,14 @@ namespace Resturant_Management_System.Entities
 {
     class User
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string BloodGroup { get; set; }
+        public string Post { get; set; }
     }
 }
