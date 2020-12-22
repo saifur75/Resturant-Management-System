49e3f22f64aa55cb176e0c6ca7ed2d8b1a36b1e9
HabiburRahmanAnik <habib264521@gmail.com>
2020-12-22 12:03:33 +0600

    Add Login and GetPost method in LoginService class

--- Resturant Management System/Resturant Management System/Resturant Logic Layer/LoginService.cs
+++ Resturant Management System/Resturant Management System/Resturant Logic Layer/LoginService.cs
@@ -1,4 +1,5 @@
 ﻿using Resturant_Management_System.Data_Access_Layer;
+using Resturant_Management_System.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,24 @@ namespace Resturant_Management_System.Resturant_Logic_Layer
 {
     class LoginService
     {
-      
+        LoginDataAccess loginDataAccess;
+
+        public LoginService()
+        {
+            this.loginDataAccess = new LoginDataAccess();
+        }
+        public bool Login(string username, string password)
+        {
+            User user = new User()
+            {
+                Username = username,
+                Password = password
+            };
+            return this.loginDataAccess.Login(user);
+        }
+        public string GetPost(string username)
+        {
+            return loginDataAccess.GetPost(username);
+        }
     }
 }
