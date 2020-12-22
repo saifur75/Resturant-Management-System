ab2c12283fee6e4519553ee56e10c698c285650e
HabiburRahmanAnik <habib264521@gmail.com>
2020-12-23 00:32:45 +0600

    Add MenuFrom

---
+++ Resturant Management System/Resturant Management System/Presentation Layer/MenuFrom.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Resturant_Management_System.Presentation_Layer
+{
+    partial class MenuFrom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MenuFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 450);
+            this.Name = "MenuFrom";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MenuFrom";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MenuFrom_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Resturant Management System/Resturant Management System/Presentation Layer/MenuFrom.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Resturant_Management_System.Presentation_Layer
+{
+    public partial class MenuFrom : Form
+    {
+        public MenuFrom()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuFrom_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
