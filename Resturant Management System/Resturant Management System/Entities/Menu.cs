5a8d993da141c268569f5862eb70b217f5e5f6a0
HabiburRahmanAnik <habib264521@gmail.com>
2020-12-22 11:36:11 +0600

    implement Menu class

--- Resturant Management System/Resturant Management System/Entities/Menu.cs
+++ Resturant Management System/Resturant Management System/Entities/Menu.cs
@@ -8,5 +8,9 @@ namespace Resturant_Management_System.Entities
 {
     class Menu
     {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Price { get; set; }
+        public String Status { get; set; }
     }
 }
