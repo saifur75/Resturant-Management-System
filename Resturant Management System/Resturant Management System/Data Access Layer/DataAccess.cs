5e683de435a10bd2787f222a5a3a07eeb7c9c019
HabiburRahmanAnik <habib264521@gmail.com>
2020-12-22 11:13:28 +0600

    implement DataAccess class

--- Resturant Management System/Resturant Management System/Data Access Layer/DataAccess.cs
+++ Resturant Management System/Resturant Management System/Data Access Layer/DataAccess.cs
@@ -1,13 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Resturant_Management_System.Data_Access_Layer
 {
-    class DataAccess
+    class DataAccess:IDisposable
     {
-        
+        SqlConnection connection;
+        SqlCommand command;
+
+        public DataAccess()
+        {
+            this.connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Test"].ConnectionString);
+            this.connection.Open();
+        }
+        public SqlDataReader GetData(string query)
+        {
+            this.command = new SqlCommand(query, this.connection);
+            SqlDataReader reader = this.command.ExecuteReader();
+            return reader;
+        }
+
+        public int ExecuteQuery(string query)
+        {
+            this.command = new SqlCommand(query, this.connection);
+            int result = this.command.ExecuteNonQuery();
+            return result;
+        }
+
+        public void Dispose()
+        {
+            this.connection.Close();
+        }
+
     }
 }
