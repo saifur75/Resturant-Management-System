f92cd852017d5aea4480c297bc9815bfffa04d3b
HabiburRahmanAnik <habib264521@gmail.com>
2020-12-22 11:43:27 +0600

    Add Login method in LoginDataAccess class

--- Resturant Management System/Resturant Management System/Data Access Layer/LoginDataAccess.cs
+++ Resturant Management System/Resturant Management System/Data Access Layer/LoginDataAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,24 @@ namespace Resturant_Management_System.Data_Access_Layer
 {
     class LoginDataAccess
     {
+        DataAccess dataAccess;
+        public LoginDataAccess()
+        {
+            this.dataAccess = new DataAccess();
+        }
+        public bool Login(string username,string password)
+        {
+            string query = "SELECT * FROM Users WHERE Username='" + username + "' and Password='" + password + "'";
+            SqlDataReader reader = this.dataAccess.GetData(query);
+
+            if (reader.Read())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
