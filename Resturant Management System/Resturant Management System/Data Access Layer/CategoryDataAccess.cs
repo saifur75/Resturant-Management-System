804be389a0d0ae06b1a3f8440a9e9fc4d0cec8dd
HabiburRahmanAnik <habib264521@gmail.com>
2020-12-23 20:08:09 +0600

    implement GetAllCategories method

--- Resturant Management System/Resturant Management System/Data Access Layer/CategoryDataAccess.cs
+++ Resturant Management System/Resturant Management System/Data Access Layer/CategoryDataAccess.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Resturant_Management_System.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,24 @@ namespace Resturant_Management_System.Data_Access_Layer
 {
     class CategoryDataAccess
     {
-
+        DataAccess dataAccess;
+        public CategoryDataAccess()
+        {
+            this.dataAccess = new DataAccess();
+        }
+        public List<Category> GetAllCategories()
+        {
+            string query = "SELECT * FROM Categories";
+            SqlDataReader reader = this.dataAccess.GetData(query);
+            List<Category> categories = new List<Category>();
+            while (reader.Read())
+            {
+                Category category = new Category();
+                category.CategoryId = (int)reader["CategoryId"];
+                category.CategoryName = reader["CategoryName"].ToString();
+                categories.Add(category);
+            }
+            return categories;
+        }
     }
 }
