1d361eb89531adf77dcb3f9d6dc54710a103192f
HabiburRahmanAnik <habib264521@gmail.com>
2020-12-23 21:38:06 +0600

    Implement MenuService

--- Resturant Management System/Resturant Management System/Resturant Logic Layer/MenuService.cs
+++ Resturant Management System/Resturant Management System/Resturant Logic Layer/MenuService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Resturant_Management_System.Data_Access_Layer;
+using Resturant_Management_System.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,48 @@ namespace Resturant_Management_System.Resturant_Logic_Layer
 {
     class MenuService
     {
+        MenuDataAccess menuDataAccess;
+
+        public MenuService()
+        {
+            this.menuDataAccess = new MenuDataAccess();
+        }
+        public List<Menu1> GetAllFood()
+        {
+            return this.menuDataAccess.GetAllFood();
+        }
+        public int AddFood(string foodname, string price, string status, string categoryName)
+        {
+            int categoryId = this.menuDataAccess.GetCategoryId(categoryName);
+            Menu1 menu = new Menu1()
+            {
+                FoodName = foodname,
+                Price =Convert.ToDouble( price),
+                Status = status,
+                CategoryId = categoryId
+            };
+            menuDataAccess = new MenuDataAccess();
+            return this.menuDataAccess.InsertFood(menu);
+        }
+        public int UpdateMenu(string foodName, double price, string status, string categoryName)
+        {
+            int categoryId = menuDataAccess.GetCategoryId(categoryName);
+            Menu1 menu = new Menu1()
+            {
+                FoodName = foodName,
+                Price = price,
+                Status = status,
+                CategoryId = categoryId
+            };
+            return this.menuDataAccess.UpdateFood(menu);
+        }
+        public int DeleteFood(int catageroyId)
+        {
+            return this.menuDataAccess.DeleteFood(catageroyId);
+        }
+        public List<Menu1> GetProductListForSearch(string productName)
+        {
+            return menuDataAccess.GetFoodForSearch(productName);
+        }
     }
 }
