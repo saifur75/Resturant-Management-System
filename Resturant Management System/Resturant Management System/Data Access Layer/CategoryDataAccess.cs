bde3a828ed10773b294f6f316b5854acb5c5cddd
HabiburRahmanAnik <habib264521@gmail.com>
2020-12-23 20:05:17 +0600

    Add CategoryDataAccess class

---
+++ Resturant Management System/Resturant Management System/Data Access Layer/CategoryDataAccess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resturant_Management_System.Data_Access_Layer
+{
+    class CategoryDataAccess
+    {
+
+    }
+}
