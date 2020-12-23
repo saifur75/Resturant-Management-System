82a28f9a55e9fe2c123bd567576f36d0fbaf6711
HabiburRahmanAnik <habib264521@gmail.com>
2020-12-23 21:24:35 +0600

    implement Search method

--- Resturant Management System/Resturant Management System/Data Access Layer/MenuDataAccess.cs
+++ Resturant Management System/Resturant Management System/Data Access Layer/MenuDataAccess.cs
@@ -58,5 +58,23 @@ namespace Resturant_Management_System.Data_Access_Layer
             dataAccess = new DataAccess();
             return this.dataAccess.ExecuteQuery(query);
         }
+        public List<Menu1> GetFoodForSearch(string foodName)
+        {
+            string sql = "SELECT * FROM Menu WHERE FoodName LIKE '" + foodName + "%'";
+            this.dataAccess = new DataAccess();
+            SqlDataReader reader = this.dataAccess.GetData(sql);
+            List<Menu1> menus = new List<Menu1>();
+            while (reader.Read())
+            {
+                Menu1 menu = new Menu1();
+                menu.FoodId = (int)reader["FoodId"];
+                menu.FoodName = reader["FoodName"].ToString();
+                menu.Price = Convert.ToDouble(reader["Price"]);
+                menu.Status = reader["Status"].ToString();
+                menu.CategoryId = (int)reader["CategoryId"];
+                menus.Add(menu);
+            }
+            return menus;
+        }
     }
 }
