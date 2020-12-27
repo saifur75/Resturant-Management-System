74e0906ae8a0e1b84f3f6fed579dc7d401c3e542
HabiburRahmanAnik <habib264521@gmail.com>
2020-12-27 23:38:49 +0600

    Implement AddOrder OrderService

--- Resturant Management System/Resturant Management System/Resturant Logic Layer/OrderService.cs
+++ Resturant Management System/Resturant Management System/Resturant Logic Layer/OrderService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Resturant_Management_System.Data_Access_Layer;
+using Resturant_Management_System.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,22 @@ namespace Resturant_Management_System.Resturant_Logic_Layer
 {
     class OrderService
     {
+        OrderDataAccess orderDataAccess;
+        public OrderService()
+        {
+            this.orderDataAccess = new OrderDataAccess();
+        }
+        public int AddOrder(string foodName,string quantity,string totalAmmount,string date)
+        {
+            Order order = new Order()
+            {
+                Food_Name = foodName,
+                Quantity = Convert.ToInt32(quantity),
+                Total_Amount = Convert.ToDouble(totalAmmount),
+                Date = date
+            };
+            orderDataAccess = new OrderDataAccess();
+            return this.orderDataAccess.AddOrder(order);
+        }
     }
 }
